22be45b66f3eff25deb95612a7bea02539668a25
OukiLove <84971861+OukiLove@users.noreply.github.com>
2022-05-29 12:23:03 +0600

    Add all file

---
+++ Loader/main.cpp
@@ -0,0 +1,57 @@
+#include <Windows.h>
+#include <iostream>
+#include "memory.hpp"
+#include <string>
+#include <format>
+#include <filesystem>
+
+#pragma comment(lib, "urlmon.lib")
+
+using namespace std;
+string namedll;
+Injector inj;
+DWORD pid;
+
+void bypass()
+{
+	LPVOID ntOpenFile = GetProcAddress(LoadLibraryW(L"ntdll"), "NtOpenFile");
+	if (ntOpenFile) {
+		char originalBytes[5];
+		memcpy(originalBytes, ntOpenFile, 5);
+		WriteProcessMemory(inj.process, ntOpenFile, originalBytes, 5, NULL);
+	}
+	else
+	{
+		cout << "Unable to bypass.\n";
+	}
+}
+
+int main()
+{
+	SetConsoleTitle("Loader By Ouki76");
+	CreateDirectory("C:\\Oreo\\", NULL);
+
+	inj.hwndproc = FindWindowA(0, "Counter-Strike: Global Offensive - Direct3D 9");
+
+	GetWindowThreadProcessId(inj.hwndproc, &pid);
+	inj.process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+
+	inj.clientDLL = inj.GetModule(pid, "client.dll");
+
+	if (pid > 0)
+	{
+		cout << "Wait..." << endl;
+		bypass();
+		string dwnld_URL = "https://github.com/OukiLove/oukilove.github.io/raw/main/assets/loader/Oreo.dll";
+		string savepath = "C:\\Oreo\\Oreo.dll";
+		URLDownloadToFile(NULL, dwnld_URL.c_str(), savepath.c_str(), 0, NULL);
+		inj.inject(pid, savepath.c_str());
+		cout << "Inject successfully\n";
+		system("pause");
+	}
+	else
+	{
+		cout << "CS:GO not found\n";
+		system("pause");
+	}
+}
